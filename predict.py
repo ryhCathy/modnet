be1c642c863b356b7f2fe1f3c35957ef52163fca
Thomas Haferlach <thomash@pollinations.ai>
2022-11-19 05:07:20 +0000

    add pollinations metadata!

--- predict.py
+++ predict.py
@@ -1,29 +1,18 @@
+import os
+
+from bg_remove import BGRemove
 from cog import BasePredictor, Input, Path
 
 
 class Predictor(BasePredictor):
     def setup(self):
         print("setup")
-
+        self.bgremover = BGRemove("/modnet_webcam_portrait_matting.ckpt")
 
     def predict(self, 
         image: Path = Input(description="input image"),
-        task_type: str = Input(
-            description='image restoration task type',
-            default='Real-World Image Super-Resolution',
-            choices=['Real-World Image Super-Resolution', 'Grayscale Image Denoising', 'Color Image Denoising','JPEG Compression Artifact Reduction']        
-        ),  
-        scale_factor: int = Input(
-            description="updscale factor for RealSR. 2 or 4 are allowed",
-            default=4),
-        jpeg: int = Input(
-            description="scale factor, activated for JPEG Compression Artifact Reduction. ",
-            default=40),
-        noise: int = Input(
-             description="noise level, activated for Grayscale Image Denoising and Color Image Denoising.",
-             default=15)
         ) -> Path:
-
-        
-        return None
+        self.bgremover.image(str(image), background=False, output='/outputs')
+        os.system("mv /outputs/*.png /outputs/output.png")
+        return Path("/outputs/output.png")
 
