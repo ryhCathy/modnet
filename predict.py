4c7d142acd89a5504ce03910963cd02b4991a19f
Thomas Haferlach <thomash@pollinations.ai>
2022-11-20 02:35:16 +0000

    add file extension to image to not confuse modnet

--- predict.py
+++ predict.py
@@ -1,5 +1,6 @@
 import os
 
+import magic
 from bg_remove import BGRemove
 from cog import BasePredictor, Input, Path
 
@@ -12,6 +13,17 @@ class Predictor(BasePredictor):
     def predict(self, 
         image: Path = Input(description="input image"),
         ) -> Path:
+        image = str(image)
+        # get mimetype
+        mime = magic.from_file(image, mime=True)
+        
+        # copy file to filename with correct extension for any type of image
+        # we should change the backend to not give filenames without an extension in the future
+        desired_extension = mime.split("/")[1]
+        new_filename = image + "." + desired_extension
+        os.system(f"cp {image} {new_filename}")
+        image = new_filename
+        
         print("running bgremover on image", str(image))
         self.bgremover.image(str(image), background=False, output='/outputs')
         os.system("ls -l /outputs")
